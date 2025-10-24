d73a44a58c7475815d6058bd46b54d83c3b5ba4e
arifeensaeed99 <108173265+arifeensaeed99@users.noreply.github.com>
2025-10-24 05:56:04 +0000

    deereai integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,69 @@
 import streamlit as st
-from openai import OpenAI
+
+from call_openai import get_ai_gateway_access_token
+from ai_gateway import DeereAIGatewayOpenAI
+import os
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Agentic Land Dashboard")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "This is a simple chatbot that uses DeereAI's OpenAI GPT-4o-mini gateway to generate responses. "
+    "It references the various Land tables in the EDL to respond to queries, with dynamic outputs."
+    "The outputs include dashboards and text."
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# try/except?
+access_token = get_ai_gateway_access_token()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+client = DeereAIGatewayOpenAI(
+    access_token=access_token,
+    base_url=os.getenv("AI_GATEWAY_BASE_URL"),
+    deere_ai_gateway_registration_id=os.getenv("AI_GATEWAY_REGISTRATION_ID")
+)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Create a session state variable to store the chat messages. This ensures that the
+# messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {
+            "role": "developer", # initial instructions for LLM behavior, thus leave as developer ; != assistant
+            "content": "You are an awesome assistant here to help users.",
+        },
+        {
+            "role": "user",
+            "content": "Hello, Deere AI!"
+        },
+    ]
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# this loops 
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+# Create a chat input field to allow the user to enter a message. This will display
+# automatically at the bottom of the page.
+if prompt := st.chat_input("Hello, Deere AI!"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+    # Generate a new response using the DeereAI gateway bearer token
+    stream = client.chat.completions.create(
+        model="gpt-4o-mini-2024-07-18",
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    # stream?
+
+    # Stream the response to the chat using `st.write_stream`, then store it in 
+    # session state.
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
